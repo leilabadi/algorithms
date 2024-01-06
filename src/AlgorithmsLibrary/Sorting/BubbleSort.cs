fa75a89e4151fa1d5a78d50e95281aff804065f7
Mostafa <leilabadi@gmail.com>
2024-01-06 04:06:44 +0000

    Improved the time complexity of bubble sort in best case scenario to linear

--- src/AlgorithmsLibrary/Sorting/BubbleSort.cs
+++ src/AlgorithmsLibrary/Sorting/BubbleSort.cs
@@ -6,13 +6,20 @@ public class BubbleSort<T> : SortBase where T : IComparable<T>
     {
         for (int i = 0; i < array.Length; i++)
         {
+            bool isSorted = true;
             for (int j = 0; j < array.Length - i - 1; j++)
             {
                 Counter++;
                 if (array[j].CompareTo(array[j + 1]) > 0)
                 {
+                    isSorted = false;
                     Swap(array, j, j + 1);
                 }
+            }
+
+            if (isSorted)
+            {
+                break;
             }
         }
         return array;
